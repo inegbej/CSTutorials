f14be11aa094b567c00d1f5051a7904190b762a4
Joseph Inegbe <josephinegbe.uk@gmail.com>
2019-02-12 17:03:15 +0000

    updated types and type members

--- CSharp.Parts/Part41_00_Labs/Part48_00_TypeAndTypeMembers.cs
+++ CSharp.Parts/Part41_00_Labs/Part48_00_TypeAndTypeMembers.cs
@@ -6,6 +6,36 @@ using System.Threading.Tasks;
 
 namespace CSharp.Parts.Part41_00_Labs
 {
+    
+    public class Customer_48
+    {       
+        private int _id;
+        private string _firstName;
+        private string _lastName;
+     
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+                
+        public string GetFullName()
+        {
+            return this._firstName + " " + this._lastName;
+        }
+        
+    }
+
     class Part48_00_TypeAndTypeMembers
     {
     }
